6e1c7395f9f4dfd44a0151943276d2e5566a0f13
Marek Siarkowicz <siarkowicz@google.com>
2025-09-30 13:21:38 +0200

    Benchmark leases

--- tools/benchmark/cmd/txn_put.go
+++ tools/benchmark/cmd/txn_put.go
@@ -16,9 +16,9 @@ package cmd
 
 import (
 	"context"
-	"encoding/binary"
 	"fmt"
 	"math"
+	"math/rand"
 	"os"
 	"time"
 
@@ -26,8 +26,10 @@ import (
 	"github.com/spf13/cobra"
 	"golang.org/x/time/rate"
 
+	clientv3 "go.etcd.io/etcd/client/v3"
 	v3 "go.etcd.io/etcd/client/v3"
 	"go.etcd.io/etcd/pkg/v3/report"
+	"go.etcd.io/etcd/pkg/v3/stringutil"
 )
 
 // txnPutCmd represents the txnPut command
@@ -39,19 +41,16 @@ var txnPutCmd = &cobra.Command{
 }
 
 var (
-	txnPutTotal     int
-	txnPutRate      int
-	txnPutOpsPerTxn int
+	txnPutTotal int
+	qpsRate     int
 )
 
 func init() {
 	RootCmd.AddCommand(txnPutCmd)
 	txnPutCmd.Flags().IntVar(&keySize, "key-size", 8, "Key size of txn put")
 	txnPutCmd.Flags().IntVar(&valSize, "val-size", 8, "Value size of txn put")
-	txnPutCmd.Flags().IntVar(&txnPutOpsPerTxn, "txn-ops", 1, "Number of puts per txn")
-	txnPutCmd.Flags().IntVar(&txnPutRate, "rate", 0, "Maximum txns per second (0 is no limit)")
+	txnPutCmd.Flags().IntVar(&qpsRate, "rate", 0, "Maximum txns per second (0 is no limit)")
 
-	txnPutCmd.Flags().IntVar(&txnPutTotal, "total", 10000, "Total number of txn requests")
 	txnPutCmd.Flags().IntVar(&keySpaceSize, "key-space-size", 1, "Maximum possible keys")
 }
 
@@ -61,53 +60,79 @@ func txnPutFunc(cmd *cobra.Command, _ []string) {
 		os.Exit(1)
 	}
 
-	if txnPutOpsPerTxn > keySpaceSize {
-		fmt.Fprintf(os.Stderr, "expected --txn-ops no larger than --key-space-size, "+
-			"got txn-ops(%v) key-space-size(%v)\n", txnPutOpsPerTxn, keySpaceSize)
+	if qpsRate <= 0 {
+		fmt.Fprintf(os.Stderr, "expected positive --rate, got (%v)", qpsRate)
 		os.Exit(1)
 	}
-
-	requests := make(chan []v3.Op, totalClients)
-	if txnPutRate == 0 {
-		txnPutRate = math.MaxInt32
-	}
-	limit := rate.NewLimiter(rate.Limit(txnPutRate), 1)
 	clients := mustCreateClients(totalClients, totalConns)
-	k, v := make([]byte, keySize), string(mustRandBytes(valSize))
+	resp, err := clients[0].Get(context.Background(), "\x00", clientv3.WithFromKey(), clientv3.WithKeysOnly())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "couldn't get all keys: %v", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Got %d keys\n", len(resp.Kvs))
+	if len(resp.Kvs) > keySpaceSize {
+		fmt.Fprintf(os.Stderr, "didn't expect keys more than in key space, got %d, want: %d", len(resp.Kvs), keySpaceSize)
+		os.Exit(1)
+	}
+	keys := []string{}
+	for _, kv := range resp.Kvs {
+		keys = append(keys, string(kv.Key))
+	}
+	if len(keys) < keySpaceSize {
+		needKeys := keySpaceSize - len(keys)
+		fmt.Printf("Need to add %d keys\n", needKeys)
+		keys = append(keys, stringutil.RandomStrings(uint(keySize), needKeys)...)
+	}
 
-	bar = pb.New(txnPutTotal)
+	value := stringutil.RandString(uint(valSize))
+	ops := make([]v3.Op, len(keys))
+	for i := range ops {
+		ops[i] = v3.OpPut(keys[i], value)
+	}
+	duration := time.Minute
+	bar = pb.New(int(duration.Seconds()) * qpsRate)
 	bar.Start()
 
-	r := newReport(cmd.Name())
+	ctx, cancel := context.WithTimeout(context.Background(), duration)
+	defer cancel()
+	takeN := int(math.Ceil(float64(qpsRate) / 500))
+	putLimiter := rate.NewLimiter(rate.Limit(qpsRate), takeN)
+	putReport := newReport(cmd.Name())
 	for i := range clients {
-		wg.Add(1)
-		go func(c *v3.Client) {
-			defer wg.Done()
-			for ops := range requests {
-				limit.Wait(context.Background())
-				st := time.Now()
-				_, err := c.Txn(context.TODO()).Then(ops...).Commit()
-				r.Results() <- report.Result{Err: err, Start: st, End: time.Now()}
-				bar.Increment()
+		c := clients[i]
+		wg.Go(func() {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+				}
+				err := putLimiter.WaitN(ctx, takeN)
+				if err != nil {
+					return
+				}
+				wg.Go(func() {
+					for range takeN {
+						select {
+						case <-ctx.Done():
+							return
+						default:
+						}
+						op := ops[rand.Intn(len(ops))]
+						start := time.Now()
+						_, err := c.Txn(context.Background()).Then(op).Commit()
+						putReport.Results() <- report.Result{Err: err, Start: start, End: time.Now()}
+						bar.Increment()
+					}
+				})
 			}
-		}(clients[i])
+		})
 	}
 
-	go func() {
-		for i := 0; i < txnPutTotal; i++ {
-			ops := make([]v3.Op, txnPutOpsPerTxn)
-			for j := 0; j < txnPutOpsPerTxn; j++ {
-				binary.PutVarint(k, int64(((i*txnPutOpsPerTxn)+j)%keySpaceSize))
-				ops[j] = v3.OpPut(string(k), v)
-			}
-			requests <- ops
-		}
-		close(requests)
-	}()
-
-	rc := r.Run()
+	putResult := putReport.Run()
 	wg.Wait()
-	close(r.Results())
 	bar.Finish()
-	fmt.Println(<-rc)
+	close(putReport.Results())
+	fmt.Println(<-putResult)
 }
